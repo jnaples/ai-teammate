9673728ed995dc17c5525bf8e165257f8313e3d4
Joe Naples <joenaples@Joes-iMac.local>
2025-06-06 16:35:18 -0400

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Montserrat, Inter } from "next/font/google";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${montserrat.variable} ${inter.variable} min-h-screen antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
